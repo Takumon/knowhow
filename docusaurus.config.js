9c194209fa04eb909c7e5bf5b1db22e39a3c8a99
Takumon <inouetakumon@gmail.com>
2023-10-12 14:55:37 +0900

    fix url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   url: 'https://takumon.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/react-training/',
+  baseUrl: '/frontend-training/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
